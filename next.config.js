665ec45dd1b1b1dd4851f9f83daa2441dabd62c8
Amreen <sulthanaa42@gmail.com>
2026-09-15 14:55:59 +1200

    fix: remove basePath for Azure deployment

--- next.config.js
+++ next.config.js
@@ -1,21 +1,14 @@
 /** @type {import('next').NextConfig} */
-
-const isProd = process.env.NODE_ENV === 'production'
-
 const nextConfig = {
-  output: 'export',
-
-  basePath: isProd ? '/demo-waikato-equitherapy' : '',
-
+  output: "export",
   trailingSlash: true,
-
   images: {
     unoptimized: true,
     remotePatterns: [
-      { protocol: 'https', hostname: '*.blob.core.windows.net' },
-      { protocol: 'https', hostname: 'placehold.co' },
+      { protocol: "https", hostname: "*.blob.core.windows.net" },
+      { protocol: "https", hostname: "placehold.co" },
     ],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
